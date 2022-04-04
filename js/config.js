2bef44b712d34a23b371f6a32dfb9fd04eea017f
WP Myanmar <89415743+wpmyanmar@users.noreply.github.com>
2022-04-04 07:31:18 +0630

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+let siteinfo  = {
+    'site': 'wpmyanmar.art.blog',
+    'name': 'WordPress Myanmar'
+};
+
+let data = `https://public-api.wordpress.com/rest/v1.1/sites/${siteinfo.site}/`;
+export {siteinfo, data};
