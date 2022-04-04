1c08e34ffcb8276b102f2f6d170779716ab170b4
WP Myanmar <89415743+wpmyanmar@users.noreply.github.com>
2022-04-04 09:04:14 +0630

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -27,8 +27,8 @@ fetch(`${data}posts/`)
         catStr = catStr.replaceAll('","description":','</a>(rm>');
         catStr = catStr.split('(rm>')[0];
 
+        // w.innerHTML += '<div class="w3-card w3-round"> <div class="article-ih"> <div class="w3-padding-small"><img src="'+q.author.avatar_URL+'" class="avatar w3-round-xxlarge w3-card"/></div><div style="width:87%; line-height: 1;">'+q.author.name+'<br/><small>'+q.date+'</small></div><div class="w3-right w3-margin-right" style="width:20%"><span class="w3-right w3-margin-righ">'+'...'+'</span></div></div><div class="w3-container">'+'<a href="article.html?id='+q.ID+'" class="plaina">'+'<p>'+q.title+'</p></a><p>Tags'+tagStr+'</rm></p></div>'+'<a href="article.html?id='+q.ID+'">'+'<img src="'+q.featured_image+'"/></a> <a href="https://wpmyanmar.github.io/" class="plaina"> <div class="w3-btn w3-bar w3-block"> Sponsored by WPMyanmar </div></a> </div><br/>';
         w.innerHTML += '<div class="w3-card w3-round"> <div class="article-ih"> <div class="w3-padding-small"><img src="'+q.author.avatar_URL+'" class="avatar w3-round-xxlarge w3-card"/></div><div style="width:87%; line-height: 1;">'+q.author.name+'<br/><small>'+q.date+'</small></div><div class="w3-right w3-margin-right" style="width:20%"><span class="w3-right w3-margin-righ">'+'...'+'</span></div></div><div class="w3-container">'+'<a href="article.html?id='+q.ID+'" class="plaina">'+'<p>'+q.title+'</p></a><p>Tags'+tagStr+'</rm></p></div>'+'<a href="article.html?id='+q.ID+'">'+'<img src="'+q.featured_image+'"/></a> <a href="https://wpmyanmar.github.io/" class="plaina"> <div class="w3-btn w3-bar w3-block"> Sponsored by WPMyanmar </div></a> </div><br/>';
-        
         
         /**
          * <div class="w3-card w3-round"> <div class="article-ih"> <div class="w3-padding-small"><img src="
